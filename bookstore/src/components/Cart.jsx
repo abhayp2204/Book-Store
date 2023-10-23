75f6ff3ac3603230319ecb13c244c46691e41022
abhayp2204 <abhaypatil1020@gmail.com>
2023-10-23 13:12:03 +0530

    cart remove book

--- bookstore/src/components/Cart.jsx
+++ bookstore/src/components/Cart.jsx
@@ -18,6 +18,7 @@ function Cart(props) {
 
 
     const updateCartCount = (bookId, newCount) => {
+        console.log("New count = ", newCount)
         // Ensure the user is authenticated
         const user = auth.currentUser;
         if (user) {
@@ -35,7 +36,9 @@ function Cart(props) {
                         const index = currentCart.findIndex(cartItem => cartItem.bookId === bookId);
     
                         if (newCount === 0) {
+                            console.log("Zero")
                             if (index !== -1) {
+                                // If the count becomes 0, remove the book from cartItems
                                 currentCart.splice(index, 1);
                             }
                         } else {
@@ -51,19 +54,21 @@ function Cart(props) {
                         // Update the user's cart in Firestore using the user document reference
                         userDoc.ref.update({ cart: currentCart })
                             .then(() => {
-                                setCartItems(currentCart)
+                                setCartItems(currentCart);
                                 console.log('Cart count updated in Firestore');
                             })
                             .catch((error) => {
                                 console.error('Error updating cart in Firestore:', error);
                             });
-                    }
+                        }
+                    
                 })
                 .catch((error) => {
                     console.error('Error fetching user details:', error);
                 });
         }
     };
+    
     
 
     // First useEffect to initialize data
@@ -92,7 +97,7 @@ function Cart(props) {
             .catch((error) => {
                 console.error('Error fetching user cart:', error);
             });
-    }, []);
+    }, [cartItems, booksData]);
 
     // Second useEffect to update book counts when userCart changes
     useEffect(() => {
@@ -120,10 +125,10 @@ function Cart(props) {
             .catch(error => {
                 console.error('Error fetching book data:', error);
             });
-    }, [cartItems]);
+    }, [cartItems, booksData]);
 
     // Calculate the total price
-    const totalPrice = booksData.reduce((total, book) => total + (book.price || 0) * book.count, 0);
+    const totalPrice = cartItems.length == 0? 0: booksData.reduce((total, book) => total + (book.price || 0) * book.count, 0);
 
     return (
         <div className="cart">
@@ -131,30 +136,34 @@ function Cart(props) {
             <p className='cart-price'>Total Price = ${totalPrice}</p> {/* Display the total price */}
             {loading ? (
                 <p>Loading...</p>
-            ) : (
+            ) : 
+            (
                 <div className='cart-container'>
-                    <div className='cart-books'>
-                        {booksData.map((book, index) => (
-                            <div className='book-details-cart' key={index}>
-                                <p className='book-title'>{book?.title}</p>
-                                {book?.image && (
-                                    <img className='book-image' src={book.image} alt={book.title} />
-                                )}
-                                <p className='book-author'>by {book?.author}</p>
-                                <p className='book-genre'>{book?.genre}</p>
-                                {/* <p className='book-desc'>{book?.description}</p> */}
-                                <p className='book-year'>{book?.publishedYear}</p>
-                                <p className='book-price'>${book?.price} (x{book.count})</p>
-                                <div className="count-buttons">
-                                    <button onClick={() => updateCartCount(book.id, book.count - 1)}>-</button>
-                                    <span>{book.count}</span>
-                                    <button onClick={() => updateCartCount(book.id, book.count + 1)}>+</button>
-                                </div>
-                                <button className='remove-button' onClick={() => removeFromCart(book.id)}>Remove</button>
-                            </div>
-                        ))}
-                    </div>
-                    {booksData.length === 0 && <p>Your cart is empty.</p>}
+                    {cartItems.length > 0 && 
+                        <div className="cart-books">
+                            {booksData.map((book, index) => (
+                                // Check if book.count is greater than 0 before rendering
+                                book.count > 0 && (
+                                    <div className='book-details-cart' key={index}>
+                                    <p className='book-title'>{book?.title}</p>
+                                    {book?.image && (
+                                        <img className='book-image' src={book.image} alt={book.title} />
+                                        )}
+                                    <p className='book-author'>by {book?.author}</p>
+                                    <p className='book-genre'>{book?.genre}</p>
+                                    <p className='book-year'>{book?.publishedYear}</p>
+                                    <p className='book-price'>${book?.price} (x{book.count})</p>
+                                    <div className="cart-counter">
+                                        <button className='counter-button' onClick={() => updateCartCount(book.id, book.count - 1)}>-</button>
+                                        <span className='counter-count'>{book.count}</span>
+                                        <button className='counter-button' onClick={() => updateCartCount(book.id, book.count + 1)}>+</button>
+                                    </div>
+                                    </div>
+                                )
+                            ))}
+                        </div>
+                    }
+                    {cartItems.length === 0 && <p>Your cart is empty.</p>}
                     <Link className='checkout-link pop' to='/checkout'>Checkout</Link>
                 </div>
             )}
